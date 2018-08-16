aab05f64b25de804b32503e87fec048fcbd1708e
fish <chuanxing@mamahao.com>
2018-08-16 10:17:18 +0800

    配置脚本

--- setup_snippets.sh
+++ setup_snippets.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
-# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
+#rm ~/Library/Developer/Xcode/UserData/CodeSnippets
 
 SRC_HOME=`pwd`
+echo "设置软连接"
+echo ${SRC_HOME}
+
 ln -s ${SRC_HOME}/codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets
 echo "done"
 
